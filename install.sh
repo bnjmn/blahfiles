025a46f0986d479f45e3a40e5c060273efa85d77
Benjamin Petersen <bnjmn@users.noreply.github.com>
2016-03-06 11:36:22 -0500

    Fix some issues with the install

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-set -x # echo cmds
 
-ln -s `pwd`/bash/bashrc $HOME/.bashrc
-ln -s `pwd`/bash/bash_profile $HOME/.bash_profile
-ln -s `pwd`/gitconfig $HOME/.gitconfig
-ln -s `pwd`/gitignore_global $HOME/.gitignore_global
-ln -s `pwd`/tmux.conf $HOME/.tmux.conf
-ln -s `pwd`/vim $HOME/.vim
-ln -s `pwd`/nvim $HOME/.config/nvim
+ln -s `pwd`/bash/bashrc "$HOME/.bashrc"
+ln -s `pwd`/bash/bash_profile "$HOME/.bash_profile"
+ln -s `pwd`/gitconfig "$HOME/.gitconfig"
+ln -s `pwd`/gitignore_global "$HOME/.gitignore_global"
+ln -s `pwd`/tmux.conf "$HOME/.tmux.conf"
+
+[ -e "$HOME/.vim" ] || ln -s `pwd`/vim "$HOME/.vim"
+[ -e "$HOME/.config/nvim" ] || ln -s `pwd`/nvim "$HOME/.config/nvim"
 
 # Plug - Pkg manager for (neo)vim
-curl -fLo `pwd`/nvim/autoload/plug.vim \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+[ ! -f `pwd`/nvim/autoload/plug.vim ] && curl -fLo `pwd`/nvim/autoload/plug.vim \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
