d849244875768d540510a5db4d9d1897d6ff6ee5
Benjamin Petersen <bnjmn@users.noreply.github.com>
2016-03-06 10:34:52 -0500

    Add bash_profile

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -x # echo cmds
 
 ln -s `pwd`/bash/bashrc $HOME/.bashrc
+ln -s `pwd`/bash/bash_profile $HOME/.bash_profile
 ln -s `pwd`/gitconfig $HOME/.gitconfig
 ln -s `pwd`/gitignore_global $HOME/.gitignore_global
 ln -s `pwd`/tmux.conf $HOME/.tmux.conf
