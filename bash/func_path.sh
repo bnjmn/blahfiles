30fe1a83cb44e8032327e31904d7df93f8c56c69
Benjamin Petersen <bnjmn@users.noreply.github.com>
2017-01-05 22:06:35 -0500

    Migrate path functions to separate file

---
+++ bash/func_path.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# --------------------------------------------------------------------
+# PATH MANIPULATION FUNCTIONS - by Ryan Tomayko
+# --------------------------------------------------------------------
+
+# Usage: pls [<var>]
+# List path entries of PATH or environment variable <var>.
+pls () { eval echo \$${1:-PATH} |tr : '\n'; }
+
+# Usage: prm <path> [<var>]
+# Remove <path> from PATH or environment variable <var>.
+prm () { eval "${2:-PATH}='$(pls $2 |grep -v "^$1\$" |tr '\n' :)'"; }
+
+# Usage: ppush <path> [<var>]
+ppush () { eval "${2:-PATH}='$(eval echo \$${2:-PATH})':$1"; }
+
+# Usage: puniq [<path>]
+# Remove duplicate entries from a PATH style value while retaining
+# the original order. Use PATH if no <path> is given.
+puniq () {
+    echo "$1" |tr : '\n' |nl |sort -u -k 2,2 |sort -n |
+    cut -f 2- |tr '\n' : |sed -e 's/:$//' -e 's/^://'
+}
