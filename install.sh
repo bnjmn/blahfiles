919a9483dd5cb4b6d417d4bd67c816c19ea2d868
Benjamin Petersen <bnjmn@users.noreply.github.com>
2017-01-23 14:33:57 -0700

    Tmux plugins and other stuff
    Tmux resurrect to the rescue

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ ln -s `pwd`/bash/bashrc "$HOME/.bashrc"
 ln -s `pwd`/bash/bash_profile "$HOME/.bash_profile"
 ln -s `pwd`/gitconfig "$HOME/.gitconfig"
 ln -s `pwd`/gitignore_global "$HOME/.gitignore_global"
-ln -s `pwd`/tmux.conf "$HOME/.tmux.conf"
 
 [ -d "$HOME/.config" ] || mkdir "$HOME/.config"
 [ -e "$HOME/.config/nvim" ] || ln -s `pwd`/nvim "$HOME/.config/nvim"
@@ -13,3 +12,8 @@ ln -s `pwd`/tmux.conf "$HOME/.tmux.conf"
 # Install Plug for managing (neo)vim packages
 PLUG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 [ ! -f `pwd`/nvim/autoload/plug.vim ] && curl -fLo `pwd`/nvim/autoload/plug.vim "$PLUG_URL"
+
+# TMUX
+ln -s `pwd`/tmux.conf "$HOME/.tmux.conf"
+[ -d "$HOME/.tmux" ] || mkdir "$HOME/.tmux"
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
