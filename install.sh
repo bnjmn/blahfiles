e59f390aa2dacfae1c21a8ac81bc670475b17575
Benjamin Petersen <git@benjaminpetersen.com>
2018-08-28 09:40:55 +0200

    Use `dir` instead of `pwd`
    just a little more portable

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 # A simple, idempotent blahfiles install
 
+blah_dir="$(cd "$(dirname "$0")" || return; pwd -P)"
+
 # link options
 ln_opts="-sfv"
 
 # make symlinks
-ln $ln_opts "`pwd`/bash/bashrc" "$HOME/.bashrc"
-ln $ln_opts "`pwd`/bash/bash_profile" "$HOME/.bash_profile"
-ln $ln_opts "`pwd`/zsh/zshrc" "$HOME/.zshrc"
+ln $ln_opts "$blah_dir/bash/bashrc" "$HOME/.bashrc"
+ln $ln_opts "$blah_dir/bash/bash_profile" "$HOME/.bash_profile"
+ln $ln_opts "$blah_dir/zsh/zshrc" "$HOME/.zshrc"
 
-ln $ln_opts "`pwd`/git/gitconfig" "$HOME/.gitconfig"
-ln $ln_opts "`pwd`/git/gitignore_global" "$HOME/.gitignore_global"
-ln $ln_opts "`pwd`/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln $ln_opts "$blah_dir/git/gitconfig" "$HOME/.gitconfig"
+ln $ln_opts "$blah_dir/git/gitignore_global" "$HOME/.gitignore_global"
+ln $ln_opts "$blah_dir/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
+# symlink asimov to automatically load it on login
+asimov_plist="com.stevegrunwell.asimov.plist"
+ln $ln_opts "$blah_dir/asimov/$asimov_plist" "$HOME/Library/LaunchAgents/$asimov_plist"
 
 # Independent vim config install
 . vim/install.sh
 
 # TMUX
-ln $ln_opts "`pwd`/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln $ln_opts "$blah_dir/tmux/tmux.conf" "$HOME/.tmux.conf"
 [ -d "$HOME/.tmux" ] || mkdir "$HOME/.tmux"
 [ -d "$HOME/.tmux/plugins/tpm" ] || git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
